e7410c546d5b1070ea1f129cb5821a41602081e0
abhishekbnair <84625363+abhishekbnair@users.noreply.github.com>
2022-07-31 17:28:41 +0530

    Update index.js

--- TODOLIST/index.js
+++ TODOLIST/index.js
@@ -97,6 +97,6 @@ app.get("/edit2/:id", function (req, res) {
   });
 });
 
-app.listen(3000, () => {
+app.listen(process.env.PORT || 3000, () => {
   console.log("Server is running on port 3000");
 });
